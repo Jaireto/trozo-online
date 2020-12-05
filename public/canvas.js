7b2d621a5cce0f46014126e31c2e7191a8af067f
Jaireto <bustamante.jair@gmail.com>
2020-12-05 00:39:09 -0300

    primer commit

---
+++ public/canvas.js
@@ -0,0 +1,9 @@
+let canv = document.getElementById('canvas-1')
+
+canv.addEventListener('click', function(){
+
+    // let canv_parent = canv.
+
+    canv.classList.remove('no-active')
+    canv.classList.add('active')
+}, false)
---
+++ script.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/', function(req, res){
+    res.sendFile('public/index.html', {root:__dirname})
+})
+
+app.get('/hola', function(req, res){
+    res.send('jairo gax')
+})
+
+app.listen(3000, function(){
+    console.log('listening on port 8080')
+})
